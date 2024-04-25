56772bf62c15bdeac8d54f2670ee8a2e6661e8a8
panteliselef <panteliselef@outlook.com>
2024-04-25 11:35:48 +0300

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,49 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import clerk from "astro-clerk-auth/hotload";
+import cloudflare from "@astrojs/cloudflare";
+import { visualizer } from "rollup-plugin-visualizer";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    tailwind(),
+    react(),
+    clerk({
+      afterSignInUrl: "/",
+      afterSignUpUrl: "/",
+    }),
+  ],
+  vite: {
+    plugins: [
+      visualizer({
+        template: "treemap",
+        open: process.env.ANALYZE === "true",
+        gzipSize: true,
+      }),
+    ],
+    ssr: {
+      external: ["async_hooks"],
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes("clerk-js")) {
+              return "@clerk-js";
+            }
+            if (id.includes("astro-clerk-auth")) {
+              return "@astro-clerk-auth";
+            }
+            if (id.includes("localizations")) {
+              return "@clerk-localizations";
+            }
+          },
+        },
+      },
+    },
+  },
+  output: "server",
+  adapter: cloudflare(),
+});
